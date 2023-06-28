c03141659c566116e8d6575ad352b7c2e71a0302
sacyad <sachinyadav4449@gmail.com>
2023-06-28 23:17:12 +0530

    palindrome changes

--- src/main/java/palindrome.java
+++ src/main/java/palindrome.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
+
 public class palindrome {
 
     public static void main(String[] args) {
-        System.out.println("the no is palindrome ...");
+
+        String orignal ,  reverse="";
+        Scanner sc =  new Scanner(System.in);
+        System.out.println("Enter number / String  to check ");
+        orignal = sc.nextLine();
+        int length =  orignal.length();
+
+        for (int i=length-1; i>=0; i--) {
+            reverse = reverse + orignal.charAt(i);
+        }
+            if (orignal.equals(reverse)){
+                System.out.println(" string or number is palindrome ");
+            }
+            else{
+            System.out.println("not palindrome .............");
+        }
     }
 }
